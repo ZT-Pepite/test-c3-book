fdd40e275c54eb0d773def87be7e76800f4887df
Omar Chebib <omar.chebib@espressif.com>
2023-09-13 12:47:23 +0800

    Fix menus with JS

--- custom.js
+++ custom.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const parentItems = document.querySelectorAll('.chapter-item.expanded');
 
-    parentItems.forEach((item) => {
-        item.addEventListener('click', () => {
-            const subMenu = item.querySelector('.chapter-item.expanded');
-            subMenu.classList.toggle('active');
-            const deployIcon = item.querySelector('.deploy');
-            deployIcon.textContent = subMenu.classList.contains('active') ? '▾' : '▸';
+    /* Retrieve all the menus name and add a class to them. As such, they will be easier to describe later */
+    document.querySelectorAll(".chapter > .chapter-item:not(.affix):has(a)").forEach(elt => {
+        elt.classList += "chapter-name";
+
+        /* Create a new HTML tag that will be added to all the menu titles */
+        const arrow = document.createElement('a');
+        arrow.innerText = '▾';
+        arrow.classList = "arrow";
+        arrow.href = "#";
+        elt.appendChild(arrow);
+
+        /* Add listeners on all arrows */
+        arrow.addEventListener('click', function(e) {
+            /* Check if the parent menu is expanded */
+            const parent = this.parentElement;
+            /* Find the index of "expanded" in the class list */
+            const expanded = parent.classList.contains("expanded");
+            /* If it is expanded, remove the class from it */
+            if (expanded) {
+                /* Remove the class from the list */
+                parent.classList.remove("expanded");
+                /* Switch the icon */
+                this.innerText = '▸';
+            } else {
+                parent.classList.add("expanded");
+                this.innerText = '▾';
+            }
         });
+
     });
+
 });
